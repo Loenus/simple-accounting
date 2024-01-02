74881ad59fce4808228433893757e46246257ef1
lorenzo <56167164+Loenus@users.noreply.github.com>
2024-01-02 20:21:37 +0100

    fix date

--- app/controllers/transaction.controller.js
+++ app/controllers/transaction.controller.js
@@ -140,7 +140,7 @@ const dateFormat = (dateString) => {
   const day = parseInt(dateParts[0], 10);
   const month = parseInt(dateParts[1], 10) - 1;
   const year = parseInt(dateParts[2], 10);
-  const dateObject = new Date(year, month, day);
+  return new Date(year, month, day);
 }
 
 
